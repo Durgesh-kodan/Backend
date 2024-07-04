805175a8172d663b9bce4b01f0078672f9f98c4c
Durgesh <durgeshkodan101@gmail.com>
2024-07-04 21:30:20 +0530

    modified user model

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -19,7 +19,7 @@ const userSchema = new Schema(
             lowercase:true,
             trim:true,
         },
-        fullname:{
+        fullName:{
             type:String , 
             required:true,
             trim:true,
@@ -52,7 +52,41 @@ const userSchema = new Schema(
 }
 );
 
-userSchema.pre("save" , )
+userSchema.pre("save" , async function next(){
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password , 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password , this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 
 export const user = mongoose.model("User" , userSchema);
