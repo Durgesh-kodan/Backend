4c9992dc5f212d4b438fb6d986c32a208e06691a
Durgesh <durgeshkodan101@gmail.com>
2024-07-07 22:25:21 +0530

    handlers and routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,16 @@
+import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
 
 const registerUser = asyncHandler(async (req , res) => {
     res.status(500).json({
         message:"response is going"
     })
+
+    const {fullName , username , email , password} = req.body;
+    if(fullName=== ""){
+        throw new ApiError(400 , "FullName is required");
+    }
+
 } )
 
 export {registerUser}
