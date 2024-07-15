74e249654bf028f8563176b9a57852fa890b9a24
Durgesh <durgeshkodan101@gmail.com>
2024-07-15 21:52:15 +0530

    logout completed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import {registerUser} from "../controllers/user.controller.js"
+import {loginUser, logoutUser, registerUser} from "../controllers/user.controller.js"
 
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
-router.route("register").post(
+router.route("/register").post(
     upload.fields([
         {
             name:"avatar",
@@ -18,5 +19,9 @@ router.route("register").post(
     ]),
     registerUser   
 )
+
+router.route("/login").post(loginUser)
+
+router.route("/logout").post(verifyJWT , logoutUser)
 
 export default router;
